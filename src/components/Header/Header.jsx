73e8a619d56e7afbb8a79f419298b109d190b084
Ultra Zacky <ultrazacky@gmail.com>
2022-11-20 23:12:37 -0800

    add next/image and contact details heading

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Image
+        src="/header.png"
+        alt="a picture of the cds building from afar"
+        width={1500}
+        height={300}
+      />
+      <div>
+        <h1>Contact Details</h1>
+      </div>
+    </header>
   )
 }
 
