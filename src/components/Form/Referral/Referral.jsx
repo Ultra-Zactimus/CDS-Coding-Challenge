141fd14f01e84d1a42562312a48c3c86f11defa3
Ultra Zacky <ultrazacky@gmail.com>
2022-11-21 00:57:58 -0800

    add inputs for referral and other and connect props

--- src/components/Form/Referral/Referral.jsx
+++ src/components/Form/Referral/Referral.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
-const Referral = () => {
+const Referral = ({ referral, submitValue, other, submitFeedback }) => {
   return (
-    <div>Referral</div>
+    <>
+      <div>
+        <select 
+          name="referral" 
+          value={referral} 
+          onChange={(e) => submitValue(e)}
+          required>
+          <option></option>
+          <option name="socialMedia" value="socialMedia">Social Media</option>
+          <option name="friend" value="friend">A Friend</option>
+          <option name="email" value="email">By Email</option>
+          <option name="other" value="other">Other</option>
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="other">
+          Please Specify 
+          <span>*</span> 
+        </label>
+        <div>
+          <textarea 
+            name="other"
+            value={other} 
+            onChange={(e) => submitFeedback(e)} 
+            required
+          >
+          </textarea>
+        </div>
+      </div>
+    </>
   )
 }
 
