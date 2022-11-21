c4bfa0b32bb8ae5bd8dfafa3d7b1ba4fd1fa2d6a
Ultra Zacky <ultrazacky@gmail.com>
2022-11-20 23:33:10 -0800

    add class names for styles for footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-      <p>For assistance please call 555-5555 or email <a href="mailto:test@test.com">test@test.com</a></p>
+    <footer className='footer'>
+      <p>For assistance please call 555-5555 or email <a href="mailto:test@test.com" className="footer__link">test@test.com</a></p>
     </footer>
   )
 }
