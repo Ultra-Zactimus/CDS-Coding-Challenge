b0ad8eb7725a94edc850e87946f9254b74d111c6
Ultra Zacky <ultrazacky@gmail.com>
2022-11-20 22:54:40 -0800

    add index for exporting components

---
+++ src/components/Form/index.js
@@ -0,0 +1,15 @@
+import Name from './Name/Name';
+import Phone from './Phone/Phone';
+import Email from './Email/Email';
+import Promo from './Promo/Promo';
+import Referral from './Referral/Referral';
+import TermsAndConditions from './TermsAndConditions/TermsAndConditions';
+
+export {
+  Name,
+  Phone,
+  Email,
+  Promo,
+  Referral,
+  TermsAndConditions
+}
