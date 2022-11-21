b3d127cfe7991b0d561a3d47be8a134764b8eeff
Ultra Zacky <ultrazacky@gmail.com>
2022-11-21 01:07:22 -0800

    add dividers, content and class names for styles

--- src/components/Form/TermsAndConditions/TermsAndConditions.jsx
+++ src/components/Form/TermsAndConditions/TermsAndConditions.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const TermsAndConditions = () => {
   return (
-    <div>TermsAndConditions</div>
+    <div className='modal__overlay'>
+      <div className='modal__container'>
+        <div className='modal__alignment'>
+          <div className='modal__content'>
+            <h2>Terms and Conditions</h2>
+            <h4>All cancellation requests must be received by March 1, 2022.</h4>
+            <h4>All cancellation requests are subject to a $100 cancellation fee.</h4>
+            <h4>No one under the age of 16 will be allowed on the show floor.</h4>
+          </div>
+          <div className='btn__modal'>
+            <button className='btn__modal--exit'>Exit</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
