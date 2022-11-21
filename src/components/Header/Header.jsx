1e46784ba6ab40899743cc656049f61b0a8a4427
Ultra Zacky <ultrazacky@gmail.com>
2022-11-20 23:16:06 -0800

    add class names for SASS styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,15 +2,15 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header>
+    <header className='header'>
       <Image
         src="/header.png"
         alt="a picture of the cds building from afar"
         width={1500}
         height={300}
       />
-      <div>
-        <h1>Contact Details</h1>
+      <div className='header__title--container'>
+        <h1 className='header__title'>Contact Details</h1>
       </div>
     </header>
   )
